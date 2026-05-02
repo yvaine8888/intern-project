06c1e11e46db4245334a94b6f22598bd758c4cb2
Yvaine Liew <yvaine8888@gmail.com>
2026-05-02 00:20:10 -0500

    Finshed most of adminfunctions except for modify.

--- banking/src/app.js
+++ banking/src/app.js
@@ -2,14 +2,126 @@ import React, { useState } from 'react';
 
 function App() {
 
-  const logging = () => {
-    setScreen('login');
-  };
-
   const [currentScreen, setCurrentScreen] = useState('role');
   const [role, setRole] = useState('');
   const [accountNum, setAccountNum] = useState('');
   const [pin, setPin] = useState('');
+  const [name, setName] = useState('');
+  const [newName, setNewName] = useState('');
+  const [newRole, setNewRole] = useState('');
+
+  const reset = () => {
+    setAccountNum() = "";
+    setPin() = "";
+    setNewName() = "";
+    setNewRole() = "";
+  }
+
+  const logging = async () => {
+    const loginInfo = {
+      role: role,
+      account: accountNum,
+      password: pin
+    }
+    try {
+      const response = await fetch('http://localhost:5000/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(loginInfo),
+      });
+      const data = await response.json();
+      if (response.ok)
+      {
+          setCurrentScreen(role);
+          setName(data.messsage);
+          document.getElementById(role + "Welcome").textContent = "Welcome, " + name + "!";
+          reset();
+      }
+      else
+      {
+        alert("Login failed.");
+      }
+    } catch (error) {
+      console.error("Error connecting to Python:", error);
+    }
+  };
+
+  const createAccount = async () => {
+    const createInfo = {
+      role: newRole,
+      name: name,
+      password: pin
+    }
+    try {
+      const response = await fetch('http://localhost:5000/api/create-account', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(createInfo),
+      });
+      const data = await response.json();
+      if (response.ok)
+      {
+          setCurrentScreen('admin');
+          alert('Created ${newRole} account ${data.messsage}');
+      }
+      else
+      {
+        alert("Failed to create account.");
+      }
+    } catch (error) {
+      console.error("Error connecting to Python:", error);
+    }
+    reset();
+  };
+
+  const closeAccount = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/close-account', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(accountNum),
+      });
+      if (response.ok)
+      {
+          setCurrentScreen('admin');
+          alert('Closed account ${accountNum}');
+      }
+      else
+      {
+        alert("Failed to close account.");
+      }
+    } catch (error) {
+      console.error("Error connecting to Python:", error);
+    }
+    reset();
+  };
+
+  const modifyAccount = async () => {
+    const modifyInfo = {
+      account: accountNum,
+      name: newName,
+      password: pin
+    }
+    try {
+      const response = await fetch('http://localhost:5000/api/modify-account', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(modifyInfo),
+      });
+      if (response.ok)
+      {
+          setCurrentScreen('admin');
+          alert('Modify account ${accountNum}');
+      }
+      else
+      {
+        alert("Failed to modify account.");
+      }
+    } catch (error) {
+      console.error("Error connecting to Python:", error);
+    }
+    reset();
+  };
 
   return (
     <main>
@@ -31,7 +143,7 @@ function App() {
           <h1>Login</h1>
           <input 
             type="text" 
-            placeholder="Enter account number" 
+            placeholder="Enter account" 
             value={accountNum} 
             onChange={(e) => setAccountNum(e.target.value)} 
           />
@@ -44,50 +156,94 @@ function App() {
           <button onClick={() => setCurrentScreen('role')}>Go Back</button>
         </section>
       )}
+
+      {currentScreen === 'admin' && (
+        <section id="adminScreen">
+          <h1 id = "adminWelcome">Welcome</h1>
+          <h1>What would you like to do?</h1>
+          <button onClick={() => setCurrentScreen('open')}> Open Account </button>
+          <button onClick={() => setCurrentScreen('close')}> Close Account </button>
+          <button onClick={() => setCurrentScreen('modify')}> modify Account </button>
+          <button onClick={() => setCurrentScreen('role')}>Logout</button>
+        </section>
+      )}
+
+      {currentScreen === 'open' && (
+        <section id="createScreen">
+          <h1>Opening Account</h1>
+          <input 
+            type="text" 
+            placeholder="Enter role" 
+            value={newRole} 
+            onChange={(e) => setNewRole(e.target.value)} 
+          />
+          <input 
+            type="text" 
+            placeholder="Enter name" 
+            value={newName} 
+            onChange={(e) => setNewName(e.target.value)} 
+          />
+        <input 
+          type="text" 
+          placeholder="Enter password" 
+          value={pin} 
+          onChange={(e) => setPin(e.target.value)} />
+          <button onClick={() => createAccount()}>Create</button>
+          <button onClick={() => setCurrentScreen('admin')}>Go Back</button>
+        </section>
+      )}
+
+      {currentScreen === 'close' && (
+        <section id="closeScreen">
+          <h1>Close Account</h1>
+        <input 
+          type="text" 
+          placeholder="Enter account number" 
+          value={accountNum} 
+          onChange={(e) => setAccountNum(e.target.value)} />
+          <button onClick={() => closeAccount()}>Enter</button>
+          <button onClick={() => setCurrentScreen('admin')}>Go Back</button>
+        </section>
+      )}
+
+      {currentScreen === 'modify' && (
+        <section id="modifyScreen">
+          <h1>Modify Account</h1>
+        <input 
+          type="text" 
+          placeholder="Enter account number" 
+          value={accountNum} 
+          onChange={(e) => setAccountNum(e.target.value)} />
+          <p>Do not need to fill in all.</p>
+        <input 
+          type="text" 
+          placeholder="Enter new name" 
+          value={newName} 
+          onChange={(e) => {
+            if (e.target.value) {
+              setNewName(e.target.value);
+            } else {
+              setNewName("");
+            }
+          }} />
+
+        <input 
+          type="text" 
+          placeholder="Enter new password" 
+          value={pin} 
+          onChange={(e) => {
+              if (e.target.value) {
+                setPin(e.target.value);
+              } else {
+                setPin("");
+              }
+            }}/>
+          <button onClick={() => modifyAccount()}>Enter</button>
+          <button onClick={() => setCurrentScreen('admin')}>Go Back</button>
+        </section>
+      )}
     </main>
   );
 }
 
 export default App;
-// document.getElementById("adminButton").addEventListener("click", function() {
-//     document.getElementById("roleScreen").style.display = "none";
-//     document.getElementById("loginScreen").style.display = "grid";
-//     localStorage.setItem("userRole", "admin");
-// });
-
-// document.getElementById("userButton").addEventListener("click", function() {
-//     document.getElementById("roleScreen").style.display = "none";
-//     document.getElementById("loginScreen").style.display = "grid";
-//     localStorage.setItem("userRole", "user");
-// });
-
-// document.getElementById("loginButton").addEventListener("click", async function() {
-//     const loginData = {
-//         bankRole: role,
-//         username: document.getElementById('accountNum').value,
-//         password: document.getElementById('pin').value
-//     };
-
-//     try {
-//         const response = await fetch('/api/login', {
-//             method: 'POST',
-//             headers: { 'Content-Type': 'application/json' },
-//             body: JSON.stringify(loginData)
-//         });
-
-//         if (response.ok) {
-//             const data = await response.json();
-//             localStorage.setItem("name", data.message);
-//             if (role == "admin"){
-//                 window.location.href = "adminFunctions.html";
-//             }
-//             else{
-//                 window.location.href = "userFunctions.html";
-//             }
-//         } else {
-//             alert("Login failed.");
-//         }
-//     } catch (error) {
-//         console.error("Connection error:", error);
-//     }
-// });
---
+++ banking/src/app.py
@@ -0,0 +1,105 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+def unique_number():
+    num = 0
+    connection = mysql.connector.connect(user = "root", database = "bankingsystem", password = "x1321PF@33")
+    cursor = connection.cursor()
+
+    while True:
+        num = random.randint(100000, 999999)
+        query = ('SELECT * FROM adminaccount WHERE pin = %s')
+        cursor.execute(query, (num))
+        if cursor.fetchone() == None:
+            query = ('SELECT * FROM adminaccount WHERE Account = %s')
+            cursor.execute(query, (num))
+            if cursor.fetchone() == None:
+                break
+    cursor.close()
+    connection.close()
+    return num
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    data = request.json 
+    
+    role = data.get('role')
+    username = data.get('account')
+    password = data.get('password')
+
+    connection = mysql.connector.connect(user = "root", database = "bankingsystem", password = "x1321PF@33")
+    cursor = connection.cursor()
+    if(role == "admin"):
+        query = ('SELECT * FROM adminaccounts WHERE name = %s AND password = %s')
+    else:
+        query = ('SELECT * FROM useraccounts WHERE name = %s AND password = %s')
+
+    cursor.execute(query, (username, password))
+    found = cursor.fetchone()
+    cursor.close()
+    connection.close()
+
+    if found != None:
+        result = {"message": found[1], "status": "ok"}
+    else:
+        result = {"error": "Invalid data"}
+    
+    return jsonify(result)
+
+@app.route('/api/create-account', methods=['POST'])
+def create():
+    try:
+        data = request.json 
+
+        role = data.get('role')
+        username = data.get('name')
+        password = data.get('password')
+
+        connection = mysql.connector.connect(user = "root", database = "bankingsystem", password = "x1321PF@33")
+        cursor = connection.cursor()
+        query = ""
+        pin = unique_number()
+        if role == 'admin':
+            query = ('INSERT INTO adminaccounts (name, pin, password) VALUES (%s, %s, %s)')
+            record = (username, pin, password, 0)
+        elif role == 'user':
+            query = ('INSERT INTO useraccounts (name, pin, password, amount) VALUES (%s, %s, %s, %s)')
+            record = (username, pin, password, 0)
+        else:
+            cursor.close()
+            connection.close()
+            return jsonify({"error": "Invalid role."})
+        cursor.execute(query, record)
+        connection.commit()
+        
+        cursor.close()
+        connection.close()
+        return jsonify({"status": "ok"})
+    except Exception as e:
+        return jsonify({"error": e})
+
+@app.route('/api/close-account', methods=['POST'])
+def close():
+    data = request.get_json()
+
+    connection = mysql.connector.connect(user = "root", database = "bankingsystem", 
+    password = "x1321PF@33")
+    cursor = connection.cursor()
+
+    query = ('DELETE FROM adminaccounts WHERE pin = %s')
+    cursor.execute(query, (data))
+    connection.commit()
+
+    query = ('DELETE FROM useraccounts WHERE pin = %s')
+    cursor.execute(query, (data))
+    connection.commit()
+    
+    cursor.close()
+    connection.close()
+
+if __name__ == '__main__':
+    app.run(debug=True)
